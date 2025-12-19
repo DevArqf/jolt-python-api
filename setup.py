07438ee5bb255447a2cebe7322305b2a218ee271
DevArqf <malik.magicly@gmail.com>
2025-12-18 23:45:59 -0400

    fix: forgot to update version :)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="Jolt-Python-API",
-    version="1.0.1",
+    version="1.2.0",
     author="DevArqf",
     author_email="devarqf@gmail.com",
     description="A Python client for the Jolt in-memory messaging broker",
